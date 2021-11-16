1dd5056030dbd473b4d5369a3fa93a2502830c05
Wei <wei@dappio.xyz>
2021-11-16 16:21:23 +0800

    comment some unused code

--- Solend/src/index.ts
+++ Solend/src/index.ts
@@ -1,19 +1,14 @@
 import {
-  AccountMeta,
   Connection,
   Keypair,
-  PublicKey,
-  sendAndConfirmTransaction,
-  Transaction,
-  TransactionInstruction,
 } from "@solana/web3.js";
 import fs from "fs";
 import os, { type } from "os";
 import * as solend from "./solend";
-const keyPairPath = os.homedir() + "/.config/solana/id.json";
+/*const keyPairPath = os.homedir() + "/.config/solana/id.json";
 const _private_key = JSON.parse(fs.readFileSync(keyPairPath, "utf-8"));
 
-const payer_keypair = new Keypair(_private_key);
+const payer_keypair = new Keypair(_private_key);*/
 
 
 
@@ -24,9 +19,9 @@ async function main() {
     "https://solana-api.projectserum.com"
   );
   const lending_markets = await solend.get_all_lending_info(connection);
-  await console.log(lending_markets);
-  //const account = await getReservedata(USDC_RESERVE, connection);
-  //console.log(account.liquidity.market_price.toLocaleString());
+  for(let market of lending_markets ){
+    console.log(market.supply_token_mint.toString(),market.supply_apy);
+  }
 }
 
 main()
