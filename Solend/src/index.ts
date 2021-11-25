adf41f6fccb1b2980a5d700d31d4650f4bd10c86
Wei <wei@dappio.xyz>
2021-11-25 23:06:27 +0800

    update testing script

--- Solend/src/index.ts
+++ Solend/src/index.ts
@@ -6,13 +6,15 @@ import {
   Ed25519Program,
   Transaction,
   sendAndConfirmTransaction,
+  PublicKey,
 } from "@solana/web3.js";
 import fs from "fs";
 import os, { type } from "os";
 import * as solend from "./solend";
-import * as obligation from "./obligation"
 import * as transaction from "./transaction"
 import { NATIVE_MINT } from "@solana/spl-token";
+import BigNumber from "bignumber.js";
+import BN from "bn.js";
 const keyPairPath = os.homedir() + "/.config/solana/id.json";
 const _private_key = JSON.parse(fs.readFileSync(keyPairPath, "utf-8"));
 const wallet = new Account(_private_key);
@@ -24,23 +26,25 @@ const wallet_public_key = wallet.publicKey;
 async function main() {
 
   const connection = new Connection(
-    "https://solana-api.projectserum.com"
+    "https://api.mainnet-beta.solana.com/"
   );
   let tx = new Transaction
   const lending_markets = await solend.get_all_lending_info(connection);
+  const positionInfo = await solend.getObligation(connection,wallet_public_key);
   for (let markets of lending_markets){
-    console.log(markets.supply_token_mint.toString(),NATIVE_MINT.toString());
+   
     if(markets.reserve_info.liquidity.mint_pubkey.toString() == NATIVE_MINT.toString()){
-      tx.add(await transaction.create_deposit_tx(markets,wallet_public_key,connection,1));
+      tx.add(await transaction.create_deposit_tx(markets,wallet_public_key,new BN(100000),connection));
+      tx.add(await transaction.createWithdrawTx(wallet_public_key,markets.reserve_address,new BN(100000),positionInfo,markets,connection));
 
     }
   }
   var recent_blockhash = (await connection.getRecentBlockhash()).blockhash;
   tx.recentBlockhash = recent_blockhash;
   tx.feePayer = wallet_public_key;
-  console.log(tx.serializeMessage().toString("base64"));
+  //console.log(tx.serializeMessage().toString("base64"));
   let result = await sendAndConfirmTransaction(connection,tx,[wallet])
-  console.log(result);
+  //console.log(result);
 }
 
 main()
