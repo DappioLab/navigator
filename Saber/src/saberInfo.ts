6cd06a5f6d8abdb3a74d50262ad27e2bedfb7c20
Wei <wei@dappio.xyz>
2021-12-06 04:07:29 +0800

    add basic saber info

---
+++ Saber/src/saberInfo.ts
@@ -0,0 +1,6 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const ADMIN_KEY = new PublicKey("H9XuKqszWYirDmXDQ12TZXGtxqUYYn4oi7FKzAm7RHGc");
+
+export const SWAP_PROGRAM_ID = new PublicKey("SSwpkEEcbUqx4vtoEByFjSkhKdCT862DNVb52nZg1UZ");
+
