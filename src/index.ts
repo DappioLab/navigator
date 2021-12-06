cfd56b2131fddc00aa294b0652859d66b43efc34
Wei <wei@dappio.xyz>
2021-12-06 19:52:57 +0800

    update test script

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import {
   } from "@solana/web3.js";
   import fs from "fs";
   import os from "os";
-  import * as solend from "../solend";
+  import * as solend from "../Solend";
   import { NATIVE_MINT } from "@solana/spl-token";
   import BN from "bn.js";
   
@@ -25,27 +25,15 @@ import {
   
   async function main() {
   
-    const connection = new Connection(
-      "https://rpc-mainnet-fork.dappio.xyz",{wsEndpoint :"https://rpc-mainnet-fork.dappio.xyz/ws"}
-    );
+    const connection = new Connection( "https://rpc-mainnet-fork.dappio.xyz",{wsEndpoint :"https://rpc-mainnet-fork.dappio.xyz/ws"});
+    //const connection = new Connection( "https://api.mainnet-beta.solana.com");
     let tx = new Transaction
     const lendingMarkets = await solend.getAllLendingInfo(connection);
     const positionInfo = await solend.getObligation(connection, walletPublicKey) as solend.Obligation ;
-    for (let markets of lendingMarkets) {
-  
-      if (markets.reserveInfo.liquidity.mintPubkey.toString() == NATIVE_MINT.toString()) {
-  
-        tx.add(await solend.createDepositTx(markets, walletPublicKey, new BN(10000000), connection));
-        tx.add(await solend.createWithdrawTx(walletPublicKey, markets.reserveAddress, new BN(1), positionInfo, markets, connection,false));
-  
-      }
-    }
-    var recentBlockhash = (await connection.getRecentBlockhash()).blockhash;
-    tx.recentBlockhash = recentBlockhash;
-    tx.feePayer = walletPublicKey;
-    console.log(tx.serializeMessage().toString("base64"),"\n");
-    let result = await sendAndConfirmTransaction(connection,tx,[wallet])
-    console.log(result);
+    console.log(positionInfo.unhealthyBorrowValue.toString())
+    positionInfo.update(lendingMarkets);
+    console.log(positionInfo.unhealthyBorrowValue.toString())
+    
   
   
   }
