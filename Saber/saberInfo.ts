217008a35668a99952c47fe08140c796e16a5dfc
Wei <wei@dappio.xyz>
2021-12-11 08:59:07 +0800

    update info add wrap program id

--- Saber/saberInfo.ts
+++ Saber/saberInfo.ts
@@ -4,3 +4,4 @@ export const ADMIN_KEY = new PublicKey("H9XuKqszWYirDmXDQ12TZXGtxqUYYn4oi7FKzAm7
 
 export const SWAP_PROGRAM_ID = new PublicKey("SSwpkEEcbUqx4vtoEByFjSkhKdCT862DNVb52nZg1UZ");
 
+export const SABER_WRAP_PROGRAM_ID = new PublicKey("DecZY86MU5Gj7kppfUCEmd4LbXXuyZH1yHaP2NTqdiZB");
