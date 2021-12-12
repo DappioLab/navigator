be6dc80e242095c003d0464c95c221dba1f54389
Wei <wei@dappio.xyz>
2021-12-13 07:58:15 +0800

    add comment and fix some smol problem

--- Saber/transaction.ts
+++ Saber/transaction.ts
@@ -19,50 +19,64 @@ import { wrapInfo } from "./wrapInfo";
 export async function createDepositTx(swapInfo: SwapInfo, AtokenAmount: BN, BtokenAmount: BN, minimalRecieve: BN, wallet: PublicKey, connection: Connection) {
     let tx: Transaction = new Transaction;
     let cleanupTx = new Transaction;
+    // check if Token A source account is created
     let AtokenSourceAccount = await findAssociatedTokenAddress(wallet, swapInfo.mintA);
     let AtokenSourceAccountCreated = await checkTokenAccount(AtokenSourceAccount, connection)
 
-    if (AtokenSourceAccountCreated) { }
-    else {
+    if (!AtokenSourceAccountCreated) {
+        //if false add a create IX
         let createAtokenAccount = await Token.createAssociatedTokenAccountInstruction(ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID, swapInfo.mintA, AtokenSourceAccount, wallet, wallet);
         tx.add(createAtokenAccount);
     }
-
+    // check if Token B source account is created
     let BtokenSourceAccount = await findAssociatedTokenAddress(wallet, swapInfo.mintB);
     let BtokenSourceAccountCreated = await checkTokenAccount(BtokenSourceAccount, connection)
-    if (BtokenSourceAccountCreated) { }
-    else {
+    if (!BtokenSourceAccountCreated) {
+        //if false add a create IX
         let createBtokenAccount = await Token.createAssociatedTokenAccountInstruction(ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID, swapInfo.mintB, BtokenSourceAccount, wallet, wallet);
         tx.add(createBtokenAccount);
     }
+    // check if LP Token account is created
     let LPtokenAccount = await findAssociatedTokenAddress(wallet, swapInfo.poolMint);
     if (await checkTokenAccount(LPtokenAccount, connection)) { }
     else {
+        //if false add a create IX
         let createAtokenAccount = await Token.createAssociatedTokenAccountInstruction(ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID, swapInfo.poolMint, LPtokenAccount, wallet, wallet);
         tx.add(createAtokenAccount);
     }
+    // check Token A is wSol
     if (swapInfo.mintA.toString() == NATIVE_MINT.toString()) {
+        // if true add a wrapNative IX
         let wrapNativeIns = await wrapNative(AtokenAmount, wallet, connection, false);
         cleanupTx.add(Token.createCloseAccountInstruction(TOKEN_PROGRAM_ID, AtokenSourceAccount, wallet, wallet, []))
         tx.add(wrapNativeIns);
     }
+    // if Token A source account is created in this tx
     else if (!AtokenSourceAccountCreated) {
+        // add a close account IX
         cleanupTx.add(Token.createCloseAccountInstruction(TOKEN_PROGRAM_ID, AtokenSourceAccount, wallet, wallet, []))
     }
+    // check Token A is wSol
     if (swapInfo.mintB.toString() == NATIVE_MINT.toString()) {
+        // if true add a wrapNative IX
         let wrapNativeIns = await wrapNative(BtokenAmount, wallet, connection, false);
         cleanupTx.add(Token.createCloseAccountInstruction(TOKEN_PROGRAM_ID, BtokenSourceAccount, wallet, wallet, []))
         tx.add(wrapNativeIns);
     }
+    // if Token B source account is created in this tx
     else if (!BtokenSourceAccountCreated) {
+        // add a close account IX
         cleanupTx.add(Token.createCloseAccountInstruction(TOKEN_PROGRAM_ID, BtokenSourceAccount, wallet, wallet, []))
     }
-    if (swapInfo.mintAWrapped == true) {
+    // if Token A is wrapped
+    if (swapInfo.mintAWrapped) {
+        // check underlying tokan account is created
         let wrapMintAtokenAddress = await findAssociatedTokenAddress(wallet, swapInfo.mintAWrapInfo?.underlyingWrappedTokenMint as PublicKey);
-        if (await checkTokenAccount(wrapMintAtokenAddress, connection)) { }
-        else {
+        if (!(await checkTokenAccount(wrapMintAtokenAddress, connection))) {
+            // if false add a create IX
             let createAtokenAccount = await Token.createAssociatedTokenAccountInstruction(ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID, swapInfo.mintAWrapInfo?.underlyingWrappedTokenMint as PublicKey, wrapMintAtokenAddress, wallet, wallet);
             tx.add(createAtokenAccount);
+            // add a close account IX
             cleanupTx.add(Token.createCloseAccountInstruction(TOKEN_PROGRAM_ID, wrapMintAtokenAddress, wallet, wallet, []))
         }
         let wrapAIns = ins.wrapToken(swapInfo.mintAWrapInfo as wrapInfo, wallet, AtokenAmount, AtokenSourceAccount, wrapMintAtokenAddress);
@@ -77,12 +91,15 @@ export async function createDepositTx(swapInfo: SwapInfo, AtokenAmount: BN, Btok
 
         tx.add(wrapAIns);
     }
+    // if Token B is wrapped
     if (swapInfo.mintBWrapped == true) {
+        // check underlying tokan account is created
         let wrapMintBtokenAddress = await findAssociatedTokenAddress(wallet, swapInfo.mintBWrapInfo?.underlyingWrappedTokenMint as PublicKey);
-        if (await checkTokenAccount(wrapMintBtokenAddress, connection)) { }
-        else {
+        if (!(await checkTokenAccount(wrapMintBtokenAddress, connection))) {
+            // if false add a create IX
             let createBtokenAccount = await Token.createAssociatedTokenAccountInstruction(ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID, swapInfo.mintBWrapInfo?.underlyingWrappedTokenMint as PublicKey, wrapMintBtokenAddress, wallet, wallet);
             tx.add(createBtokenAccount);
+            // add a close account IX
             cleanupTx.add(Token.createCloseAccountInstruction(TOKEN_PROGRAM_ID, wrapMintBtokenAddress, wallet, wallet, []))
         }
         let wrapBIns = ins.wrapToken(swapInfo.mintBWrapInfo as wrapInfo, wallet, BtokenAmount, wrapMintBtokenAddress, BtokenSourceAccount);
