5b5801b179e64513a860e06bbea0272d58914a9a
Wei <wei@dappio.xyz>
2021-12-14 19:33:15 +0800

    update test script

--- index.ts
+++ index.ts
@@ -14,8 +14,7 @@ import {
   import * as saber from "./Saber";
   import { NATIVE_MINT } from "@solana/spl-token";
   import BN from "bn.js";
-  
- 
+  import * as util from "./util"
   const keyPairPath = os.homedir() + "/.config/solana/id.json";
   const PrivateKey = JSON.parse(fs.readFileSync(keyPairPath, "utf-8"));
   const wallet = new Account(PrivateKey);
@@ -32,7 +31,46 @@ import {
     let tx = new Transaction
     let swap = await saber.getAllSwap(connection);
     console.log(swap);
+    for (let info of swap) {
+      if (info.mintB.toString()== NATIVE_MINT.toString()) {
+        
+        let tx = new Transaction
+        await info.updateAmount(connection);
+        
+        let deposit = await saber.createDepositTx(info,new BN(0),new BN(1000000),new BN(0),walletPublicKey,connection);
+        tx.add(deposit);
+
+        /*let amount = new BN(0)
+        
+        let withdrawAccount = await util.findAssociatedTokenAddress(walletPublicKey,info.poolMint);
+        if ( !(await util.checkTokenAccount(withdrawAccount,connection))){
+          continue;
+        }
+        amount =new BN( await util.getTokenAccountAmount(connection,withdrawAccount))
+        if (amount.eq(new BN(0))){
+          continue;
+        }
+        let withdraw = await saber.createWithdrawTx(info, "B", amount, new BN(0), walletPublicKey, connection);
+        tx.add(withdraw);*/
   
+  
+        var recentBlockhash = (await connection.getRecentBlockhash()).blockhash;
+        tx.recentBlockhash = recentBlockhash;
+        tx.feePayer = walletPublicKey;
+        console.log(tx.serializeMessage().toString("base64"), "\n");
+        //let simulation = await connection.simulateTransaction(tx.compileMessage(), [wallet])
+        //console.log(simulation.value.err);
+  
+        let result = await sendAndConfirmTransaction(connection, tx, [wallet])
+        console.log(result);
+  
+  
+  
+  
+      } 
+      
+      
+    }
   
   }
   main()
