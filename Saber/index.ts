a0e56cf541215439d86d2abc175acbd86ca0b2a7
Wei <wei@dappio.xyz>
2021-12-16 19:56:39 +0800

    add claim test script

--- Saber/index.ts
+++ Saber/index.ts
@@ -1,6 +1,6 @@
 
 
 export { getAllSwap } from './saber'
-export { createDepositTx ,createWithdrawTx,depositToFarm} from './transaction'
+export { createDepositTx ,createWithdrawTx,depositToFarm,claimRewardTx} from './transaction'
 export { getAllMiner } from './minerLayout'
 export { FarmInfo } from './farmInfoLayout'
