1651f15106325418e7fb93082906b173de4ca41a
Wei <wei@dappio.xyz>
2021-12-30 00:29:03 +0800

    add katana address

---
+++ Katana/info.ts
@@ -0,0 +1,5 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const KATANA_PROGRAM_ID = new PublicKey("1349iiGjWC7ZTbu6otFmJwztms122jEEnShKgpVnNewy")
+
+export const ADMIN = new PublicKey("CSRpF4yUaG55gWm82StQiguFerdq5VSByqHd6i8t5YxK")
