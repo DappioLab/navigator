06debb692e75e740404ba122457d437d8c7237f7
Wei <wei@dappio.xyz>
2022-01-07 07:40:16 +0800

    add getOtcTermsAccount to import

--- Katana/coverCall/index.ts
+++ Katana/coverCall/index.ts
@@ -3,3 +3,4 @@ export { getAllVault,getVault,getAllUserVault, } from './katana'
 export { getUserVaultAddress } from "./userVault"
 export {getPricePerPageAccount} from './pricePerPage'
 export { deposit,instantWithdraw, initiateWithdraw,completeWithdraw ,claimShares} from './transaction'
+export {getOtcTermsAccount} from './otcTermsInfo'
