746e394aacb2f48cd6c54acd610be313e0454373
Wei <wei@dappio.xyz>
2022-01-07 08:38:57 +0800

    add putSell program SDK

---
+++ Katana/putSell/index.ts
@@ -0,0 +1,6 @@
+export { getAllVault,getVault,getAllUserVault, } from './katana'
+
+export { getUserVaultAddress } from "./userVault"
+export {getPricePerPageAccount} from './pricePerPage'
+export { deposit,instantWithdraw, initiateWithdraw,completeWithdraw ,claimShares} from './transaction'
+export {getOtcTermsAccount} from './otcTermsInfo'
---
+++ Katana/putSell/otcTermsInfo.ts
@@ -0,0 +1,67 @@
+import { publicKey, struct, u64, u128, u8, bool, u16, i64 } from "@project-serum/borsh";
+import { Connection, PublicKey } from "@solana/web3.js";
+import BN from "bn.js";
+import { KATANA_PROGRAM_ID } from "./info";
+import { Vault } from "./vaultInfo";
+
+interface OtcTermInterface {
+    infoPubkey: PublicKey;
+    round: BN;
+    totalPrice: BN;
+    tokenMintToBuy: PublicKey;
+    tokenMintToSell: PublicKey;
+    bump: BN;
+}
+
+export class OtcTerms implements OtcTermInterface {
+    infoPubkey: PublicKey;
+    round: BN;
+    totalPrice: BN;
+    tokenMintToBuy: PublicKey;
+    tokenMintToSell: PublicKey;
+    bump: BN;
+    constructor(
+        infoPubkey: PublicKey,
+        round: BN,
+        totalPrice: BN,
+        tokenMintToBuy: PublicKey,
+        tokenMintToSell: PublicKey,
+        bump: BN,
+    ) {
+        this.infoPubkey = infoPubkey;
+        this.round = new BN(round);
+        this.totalPrice = new BN(totalPrice);
+        this.tokenMintToBuy = tokenMintToBuy;
+        this.tokenMintToSell = tokenMintToSell;
+        this.bump = new BN(bump);
+    }
+}
+
+export const OTC_TERMS_LAYOUT = struct([
+    u128("round"),
+    u64("totalPrice"),
+    publicKey("tokenMintToBuy"),
+    publicKey("tokenMintToSell"),
+    u8("bump"),
+
+]);
+export function parceOtcTermsData(data: any, infoPubkey: PublicKey) {
+    let dataBuffer = data as Buffer;
+    let otcData = dataBuffer.slice(8);
+    let otcRaw = OTC_TERMS_LAYOUT.decode(otcData);
+    let { round, totalPrice, tokenMintToBuy, tokenMintToSell, bump } = otcRaw;
+
+    return new OtcTerms(infoPubkey, round, totalPrice, tokenMintToBuy, tokenMintToSell, bump)
+}
+export async function getOtcTermsAccount(vault: Vault, connection: Connection) {
+    let infoPubkey = await vault.getOtcTerms()
+    let account = await connection.getAccountInfo(infoPubkey);
+    if(account == undefined){
+        return defaultOtcTerms(infoPubkey)
+    }
+    let otc = parceOtcTermsData(account?.data, infoPubkey);
+    return otc;
+}
+export function defaultOtcTerms(infoPubkey: PublicKey) {
+   return new OtcTerms(infoPubkey, new BN(0), new BN(0), PublicKey.default, PublicKey.default, new BN(0))
+}
