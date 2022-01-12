61e0faed928760d4ec5354b84ffdf6aadaf7eeca
Wei <wei@dappio.xyz>
2022-01-12 20:48:29 +0800

    Fix create user vault instruction change a way to convert number to Buffer

--- Katana/coverCall/instructions.ts
+++ Katana/coverCall/instructions.ts
@@ -28,8 +28,8 @@ export async function createUserVaultIx(vault: Vault, wallet: PublicKey) {
     const dataLayout = struct([
         u8('bump'),
     ]);
-    let data = new BN(userVault[1]).toBuffer("le",1)
-    let datahex = data.toString('hex');
+    let data = Buffer.alloc(9)
+    let datahex = userVault[1].toString(16)
     let datastring = '924464453f2eb6c7'.concat(datahex);
     data = Buffer.from(datastring, "hex")
 
