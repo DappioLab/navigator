8d6cacfd07f48655d29bc34bbb84ada9ebf7f7d5
Wei <wei@dappio.xyz>
2022-01-14 11:56:00 +0800

    add orca, raydium constant

---
+++ Francium/raydium/info.ts
@@ -0,0 +1,3 @@
+import { PublicKey } from "@solana/web3.js";
+export const lyfRaydiumProgramId = new PublicKey("2nAAsYdXF3eTQzaeUQS3fr4o782dDg8L28mX39Wr5j8N");
+export const ADMIN = new PublicKey("7MBLg6oV5phip11YBbJPuq7u38kdzSi9PM3BifKSpLaR")
