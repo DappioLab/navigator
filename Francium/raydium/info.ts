7b7950a8117a492ccb3c4120823354bf58d1022c
Wei <wei@dappio.xyz>
2022-01-25 08:57:35 +0800

    update constants

--- Francium/raydium/info.ts
+++ Francium/raydium/info.ts
@@ -1,3 +1,6 @@
 import { PublicKey } from "@solana/web3.js";
 export const lyfRaydiumProgramId = new PublicKey("2nAAsYdXF3eTQzaeUQS3fr4o782dDg8L28mX39Wr5j8N");
 export const ADMIN = new PublicKey("7MBLg6oV5phip11YBbJPuq7u38kdzSi9PM3BifKSpLaR")
+export const LENDING_MARKET = new PublicKey("4XNif294wbrxj6tJ8K5Rg7SuaEACnu9s2L27i28MQB6E")
+export const LENDING_AUTHORITY = new PublicKey("sCDiYj7X7JmXg5fVq2nqED2q1Wqjo7PnqMgH3casMem")
+export const lendProgramId = new PublicKey("FC81tbGt6JWRXidaWYFXxGnTk4VgobhJHATvTRVMqgWj");
