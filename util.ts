91da890a7b5f7cd8901e289b4d2a99665546fe25
Wei <wei@dappio.xyz>
2022-01-25 08:56:05 +0800

    update util

--- util.ts
+++ util.ts
@@ -4,7 +4,7 @@ import {
   ASSOCIATED_TOKEN_PROGRAM_ID,
   Token,
 } from "@solana/spl-token";
-
+import * as sha256 from "js-sha256"
 import {
   PublicKey,
   SYSVAR_CLOCK_PUBKEY,
@@ -172,4 +172,15 @@ export class TokenAccount{
     this.owner = owner;
     this.amount = new BN(amount);
   }
+}
+
+export function getAnchorInsByIdl(name: string, old?: boolean) {
+  const SIGHASH_GLOBAL_NAMESPACE = "global";
+  let preimage = `${SIGHASH_GLOBAL_NAMESPACE}::${name}`;
+  if (!old){
+     preimage = `${SIGHASH_GLOBAL_NAMESPACE}:${name}`;
+  }
+  let hash = sha256.sha256.digest(preimage)
+  let data = Buffer.from(hash).slice(0,8)
+  return data;
 }
