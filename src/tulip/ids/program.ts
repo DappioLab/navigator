1059177ab6d16ee2f1cadac1cc7eeca810d5af42
emerson <emerson@dappio.xyz>
2022-09-07 22:06:54 +0800

    Update ids

---
+++ src/tulip/ids/program.ts
@@ -0,0 +1,7 @@
+import { PublicKey } from "@solana/web3.js";
+
+// Program ID
+
+export const TULIP_PROGRAM_ID = new PublicKey("4bcFeLv4nydFrsZqV5CgwCVrPhkQKsXtzfy2KyMz7ozM");
+
+export const TULIP_VAULT_V2_PROGRAM_ID = new PublicKey("TLPv2tuSVvn3fSk8RgW3yPddkp5oFivzZV3rA9hQxtX");
