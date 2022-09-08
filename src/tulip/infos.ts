872015bc49971fb84ea282fe358b75a8af4c3fd1
happyeric77 <happyeric77@gmail.com>
2022-09-08 14:24:10 +0900

    Add depositor and clean up

--- src/tulip/infos.ts
+++ src/tulip/infos.ts
@@ -1,7 +1,7 @@
 import { getMint } from "@solana/spl-token-v2";
 import { Connection, PublicKey, GetProgramAccountsConfig, MemcmpFilter, DataSizeFilter } from "@solana/web3.js";
 import BN from "bn.js";
-import { IDepositorInfo, IInstanceMoneyMarket, IInstanceVault, IReserveInfoWrapper, IVaultInfoWrapper } from "../types";
+import { IInstanceMoneyMarket, IInstanceVault, IReserveInfoWrapper, IVaultInfoWrapper } from "../types";
 import { vaultV2Config, TULIP_PROGRAM_ID, TULIP_VAULT_V2_PROGRAM_ID } from "./ids";
 import { DEPOSITOR_LAYOUT, RAYDIUM_VAULT_LAYOUT, RESERVE_LAYOUT } from "./layout";
 import * as types from ".";
@@ -30,13 +30,6 @@ infos = class InstanceTulip {
     const reserveAccounts = await connection.getProgramAccounts(TULIP_PROGRAM_ID, config);
 
     const reserves = reserveAccounts.map((account) => this.parseReserve(account.account.data, account.pubkey));
-
-    // let reserves = [] as types.ReserveInfo[];
-    // for (let account of reserveAccounts) {
-    //   let info = this.parseReserve(account.account.data, account.pubkey);
-    //   reserves.push(info);
-    // }
-
     return reserves;
   }
 
@@ -44,13 +37,6 @@ infos = class InstanceTulip {
     const reserves = await this.getAllReserves(connection, marketId);
     const reserveWrappers = reserves.map((reserve) => new ReserveInfoWrapper(reserve));
     return reserveWrappers;
-
-    // const allReserves = await this.getAllReserves(connection, marketId);
-    // const allReservesWrapper: ReserveInfoWrapper[] = [];
-    // allReserves.map((reserveInfo) => {
-    //   allReservesWrapper.push(new ReserveInfoWrapper(reserveInfo));
-    // });
-    // return allReservesWrapper;
   }
 
   static async getReserve(connection: Connection, reserveId: PublicKey): Promise<types.ReserveInfo> {
@@ -87,17 +73,6 @@ infos = class InstanceTulip {
       .filter((info) => this._isAllowedId(info.pubkey))
       .map((info) => this.parseVault(info.account.data, info.pubkey));
     return vaults;
-
-    // const allVaultAccount = await connection.getProgramAccounts(TULIP_VAULT_V2_PROGRAM_ID, config);
-    // let allVault: types.VaultInfo[] = [];
-
-    // for (let vaultAccountInfo of allVaultAccount) {
-    //   if (this._isAllowedId(vaultAccountInfo.pubkey)) {
-    //     const vault = this.parseVault(vaultAccountInfo.account.data, vaultAccountInfo.pubkey);
-    //     allVault.push(vault);
-    //   } else continue;
-    // }
-    // return allVault;
   }
 
   static async getVault(connection: Connection, vaultId: PublicKey): Promise<types.VaultInfo> {
@@ -119,18 +94,69 @@ infos = class InstanceTulip {
         parseVault = this._parseRaydiumVault;
     }
     return parseVault(data, vaultId);
-    // DEBUG
-    // let vault = {} as unknown as types.VaultInfo;
-    // let failId = 0;
-    // let successId = 0;
-    // try {
-    //   vault = parseVault(data, vaultId);
-    //   console.log({ successId, vaultId: vaultId.toBase58() });
-    // } catch (e) {
-    //   failId++;
-    //   console.log({ failId, vaultId: vaultId.toBase58() });
-    // }
-    // return vault;
+  }
+
+  static async getAllDepositors(connection: Connection, userKey: PublicKey): Promise<types.DepositorInfo[]> {
+    const dataSizeFilters: DataSizeFilter = {
+      dataSize: DEPOSITOR_LAYOUT_SPAN,
+    };
+    let filters: (MemcmpFilter | DataSizeFilter)[] = [dataSizeFilters];
+    if (userKey) {
+      const programIdMemcmp: MemcmpFilter = {
+        memcmp: {
+          offset: 8,
+          bytes: userKey.toString(),
+        },
+      };
+      filters = [programIdMemcmp, dataSizeFilters];
+    }
+    const config: GetProgramAccountsConfig = { filters: filters };
+
+    const accountInfos = await connection.getProgramAccounts(TULIP_VAULT_V2_PROGRAM_ID, config);
+    const depositors = accountInfos.map((depositor) => this.parseDepositor(depositor.account.data, depositor.pubkey));
+    return depositors;
+  }
+
+  static async getDepositor(connection: Connection, depositorId: PublicKey): Promise<types.DepositorInfo> {
+    const depositorAccountInfo = await connection.getAccountInfo(depositorId);
+    if (!depositorAccountInfo) throw Error("Error: Cannot get depositor account (tulip.getDepositor)");
+    return this.parseDepositor(depositorAccountInfo.data, depositorId);
+  }
+
+  static parseDepositor(data: Buffer, depositorId: PublicKey): types.DepositorInfo {
+    const decodeData = DEPOSITOR_LAYOUT.decode(data);
+    const {
+      owner,
+      vault,
+      pdaNonce,
+      queueNonce,
+      shares,
+      depositedBalance,
+      lastDepositTime,
+      pendingWithdrawAmount,
+      totalDepositedUnderlying,
+      totalWithdrawnUnderlying,
+      lastPendingReward,
+      rewardPerSharePaid,
+      extra_data_account,
+    } = decodeData;
+
+    return {
+      depositorId,
+      userKey: owner,
+      vaultId: vault,
+      pdaNonce,
+      queueNonce,
+      shares,
+      depositedBalance,
+      lastDepositTime,
+      pendingWithdrawAmount,
+      totalDepositedUnderlying,
+      totalWithdrawnUnderlying,
+      lastPendingReward,
+      rewardPerSharePaid,
+      extra_data_account,
+    };
   }
 
   private static _parseRaydiumVault(data: any, vaultId: PublicKey): types.RaydiumVaultInfo {
@@ -212,18 +238,6 @@ infos = class InstanceTulip {
       ? true
       : false;
   }
-
-  static async getAllDepositors(connection: Connection, userKey: PublicKey): Promise<IDepositorInfo[]> {
-    return [];
-  }
-
-  static async getDepositor(connection: Connection, depositorId: PublicKey): Promise<IDepositorInfo> {
-    return {} as IDepositorInfo;
-  }
-
-  static parseDepositor(data: Buffer, depositorId: PublicKey): IDepositorInfo {
-    return {} as IDepositorInfo;
-  }
 };
 
 export { infos };
@@ -299,388 +313,4 @@ export class VaultInfoWrapper implements IVaultInfoWrapper {
       TULIP_VAULT_V2_PROGRAM_ID
     );
   }
-}
-
-// export async function getAllVaults(connection: Connection): Promise<VaultInfo[]> {
-//   const sizeFilter: DataSizeFilter = {
-//     dataSize: RAYDIUM_VAULT_V1_LAYOUT_SPAN,
-//   };
-//   const filters = [sizeFilter];
-//   const config: GetProgramAccountsConfig = { filters: filters };
-//   const allVaultAccount = await connection.getProgramAccounts(TULIP_VAULT_V2_PROGRAM_ID, config);
-//   let allVault: VaultInfo[] = [];
-
-//   for (let vaultAccountInfo of allVaultAccount) {
-//     let parseVault;
-//     switch (vaultAccountInfo.account.data.length) {
-//       case RAYDIUM_VAULT_V1_LAYOUT_SPAN:
-//         parseVault = parseRaydiumVault;
-//         break;
-//       default:
-//         parseVault = parseRaydiumVault;
-//     }
-
-//     const vault = parseVault(vaultAccountInfo.account.data, vaultAccountInfo.pubkey);
-//     allVault.push(vault);
-//   }
-
-//   return allVault;
-// }
-
-// export async function getVault(connection: Connection, vaultId: PublicKey): Promise<VaultInfo> {
-//   const vaultAccountInfo = await connection.getAccountInfo(vaultId);
-
-//   let parseVault;
-//   switch (vaultAccountInfo?.data.length) {
-//     case RAYDIUM_VAULT_V1_LAYOUT_SPAN:
-//       parseVault = parseRaydiumVault;
-//       break;
-//     default:
-//       parseVault = parseRaydiumVault;
-//   }
-
-//   const vault = parseVault(vaultAccountInfo?.data, vaultId);
-
-//   return vault;
-// }
-
-// export function parseRaydiumVault(data: any, vaultId: PublicKey): RaydiumVaultInfo {
-//   const decodeData = RAYDIUM_VAULT_LAYOUT.decode(data);
-//   const {
-//     base,
-//     raydiumLpMintAddress,
-//     raydiumAmmId,
-//     raydiumAmmAuthority,
-//     raydiumAmmOpenOrders,
-//     raydiumAmmQuantitiesOrTargetOrders,
-//     raydiumStakeProgram,
-//     raydiumLiquidityProgram,
-//     raydiumCoinTokenAccount,
-//     raydiumPcTokenAccount,
-//     raydiumPoolTempTokenAccount,
-//     raydiumPoolLpTokenAccount,
-//     raydiumPoolWithdrawQueue,
-//     raydiumPoolId,
-//     raydiumPoolAuthority,
-//     raydiumPoolRewardATokenAccount,
-//     raydiumPoolRewardBTokenAccount,
-//     dualRewards,
-//     vaultRewardATokenAccount,
-//     vaultRewardBTokenAccount,
-//     vaultStakeInfoAccount,
-//     associatedStakeInfoAddress,
-//     coinMint,
-//     pcMint,
-//     serumMarket,
-//   } = decodeData;
-
-//   const vaultAccount = vaultV2Config.vaults.accounts.find((account) => account.raydium?.account == vaultId.toString());
-
-//   return {
-//     vaultId,
-//     shareMint: base.sharesMint,
-//     base,
-//     lpMint: raydiumLpMintAddress,
-//     ammId: raydiumAmmId,
-//     ammAuthority: raydiumAmmAuthority,
-//     ammOpenOrders: raydiumAmmOpenOrders,
-//     ammQuantitiesOrTargetOrders: raydiumAmmQuantitiesOrTargetOrders,
-//     stakeProgram: raydiumStakeProgram,
-//     liquidityProgram: raydiumLiquidityProgram,
-//     coinTokenAccount: raydiumCoinTokenAccount,
-//     pcTokenAccount: raydiumPcTokenAccount,
-//     poolTempTokenAccount: raydiumPoolTempTokenAccount,
-//     poolLpTokenAccount: raydiumPoolLpTokenAccount,
-//     poolWithdrawQueue: raydiumPoolWithdrawQueue,
-//     poolId: raydiumPoolId,
-//     poolAuthority: raydiumPoolAuthority,
-//     poolRewardATokenAccount: raydiumPoolRewardATokenAccount,
-//     poolRewardBTokenAccount: raydiumPoolRewardBTokenAccount,
-//     feeCollectorRewardATokenAccount: new PublicKey(vaultAccount?.raydium?.fee_collector_reward_a_token_account!),
-//     feeCollectorRewardBTokenAccount: new PublicKey(vaultAccount?.raydium?.fee_collector_reward_b_token_account!),
-//     dualRewards,
-//     vaultRewardATokenAccount,
-//     vaultRewardBTokenAccount,
-//     vaultStakeInfoAccount,
-//     associatedStakeInfoAddress,
-//     coinMint,
-//     pcMint,
-//     serumMarket,
-//   };
-// }
-
-// interface Fees {
-//   feeMultiplier: BN;
-//   controllerFee: BN;
-//   platformFee: BN;
-//   withdrawFee: BN;
-//   depositFee: BN;
-//   feeWallet: PublicKey;
-//   totalCollectedA: BN;
-//   totalCollectedB: BN;
-// }
-
-// interface RealizedYield {
-//   gainPerSecond: BN;
-//   apr: BN;
-// }
-
-// interface Base {
-//   nonce: BN;
-//   tag: BN[];
-//   pda: PublicKey;
-//   pdaNonce: BN;
-//   pdaAlignment: BN[];
-//   totalDepositedBalance: BN;
-//   totalShares: BN;
-//   underlyingMint: PublicKey;
-//   underlyingWithdrawQueue: PublicKey;
-//   underlyingDepositQueue: PublicKey;
-//   underlyingCompoundQueue: PublicKey;
-//   sharesMint: PublicKey;
-//   withdrawsPaused: BN;
-//   depositsPaused: BN;
-//   compoundPaused: BN;
-//   supportsCompound: BN;
-//   rebasePaused: BN;
-//   rebalancePaused: BN;
-//   stateAlignment: BN[];
-//   precisionFactor: BN;
-//   lastCompoundTime: BN;
-//   compoundInterval: BN;
-//   slippageTolerance: BN;
-//   slipAlignment: BN[];
-//   fees: Fees;
-//   farm: BN[];
-//   configured: BN;
-//   configuredAlignment: BN[];
-//   pendingFees: BN;
-//   totalDepositedBalanceCap: BN;
-//   realizedYield: RealizedYield;
-// }
-
-// export interface RaydiumVaultInfo extends VaultInfo {
-//   base: Base;
-//   lpMint: PublicKey;
-//   ammId: PublicKey;
-//   ammAuthority: PublicKey;
-//   ammOpenOrders: PublicKey;
-//   ammQuantitiesOrTargetOrders: PublicKey;
-//   stakeProgram: PublicKey;
-//   liquidityProgram: PublicKey;
-//   coinTokenAccount: PublicKey;
-//   pcTokenAccount: PublicKey;
-//   poolTempTokenAccount: PublicKey;
-//   poolLpTokenAccount: PublicKey;
-//   poolWithdrawQueue: PublicKey;
-//   poolId: PublicKey;
-//   poolAuthority: PublicKey;
-//   poolRewardATokenAccount: PublicKey;
-//   poolRewardBTokenAccount: PublicKey;
-//   feeCollectorRewardATokenAccount: PublicKey;
-//   feeCollectorRewardBTokenAccount: PublicKey;
-//   dualRewards: BN;
-//   vaultRewardATokenAccount: PublicKey;
-//   vaultRewardBTokenAccount: PublicKey;
-//   vaultStakeInfoAccount: PublicKey;
-//   associatedStakeInfoAddress: PublicKey;
-//   coinMint: PublicKey;
-//   pcMint: PublicKey;
-//   serumMarket: PublicKey;
-// }
-
-// export async function getAllReserveWrappers(
-//   connection: Connection,
-//   lendingMarket?: PublicKey
-// ): Promise<ReserveInfoWrapper[]> {
-//   const allReserves = await getAllReserves(connection, lendingMarket);
-//   const allReservesWrapper: ReserveInfoWrapper[] = [];
-
-//   allReserves.map((reserveInfo) => {
-//     allReservesWrapper.push(new ReserveInfoWrapper(reserveInfo));
-//   });
-
-//   return allReservesWrapper;
-// }
-
-// export async function getAllReserves(connection: Connection, lendingMarket?: PublicKey): Promise<ReserveInfo[]> {
-//   const dataSizeFilters: DataSizeFilter = {
-//     dataSize: RESERVE_LAYOUT_SPAN,
-//   };
-
-//   let filters: any[] = [dataSizeFilters];
-//   if (lendingMarket) {
-//     const programIdMemcmp: MemcmpFilter = {
-//       memcmp: {
-//         //offset 10 byte
-//         offset: 10,
-//         bytes: lendingMarket.toString(),
-//       },
-//     };
-//     filters = [programIdMemcmp, dataSizeFilters];
-//   }
-
-//   const config: GetProgramAccountsConfig = { filters: filters };
-//   const reserveAccounts = await connection.getProgramAccounts(TULIP_PROGRAM_ID, config);
-//   let reserves = [] as ReserveInfo[];
-//   for (let account of reserveAccounts) {
-//     let info = parseReserveData(account.account.data, account.pubkey);
-//     reserves.push(info);
-//   }
-
-//   return reserves;
-// }
-
-// export async function getReserve(connection: Connection, reserveId: PublicKey): Promise<ReserveInfo> {
-//   const reserveAccountInfo = await connection.getAccountInfo(reserveId);
-//   return parseReserveData(reserveAccountInfo?.data, reserveId);
-// }
-
-// export async function getLendingMarketAuthority(lendingMarket: PublicKey): Promise<PublicKey> {
-//   const authority = (await PublicKey.findProgramAddress([lendingMarket.toBuffer()], TULIP_PROGRAM_ID))[0];
-
-//   return authority;
-// }
-
-// export function parseReserveData(data: any, pubkey: PublicKey): ReserveInfo {
-//   const decodedData = RESERVE_LAYOUT.decode(data);
-//   let { version, lastUpdate, lendingMarket, borrowAuthorizer, liquidity, collateral, config } = decodedData;
-
-//   return {
-//     reserveId: pubkey,
-//     version,
-//     lastUpdate,
-//     lendingMarket,
-//     borrowAuthorizer,
-//     liquidity,
-//     collateral,
-//     config,
-//   };
-// }
-
-// interface ReserveConfig {
-//   optimalUtilizationRate: BN;
-//   degenUtilizationRate: BN;
-//   loanToValueRatio: BN;
-//   liquidationBonus: BN;
-//   liquidationThreshold: BN;
-//   minBorrowRate: BN;
-//   optimalBorrowRate: BN;
-//   degenBorrowRate: BN;
-//   maxBorrowRate: BN;
-//   fees: ReserveFees;
-// }
-
-// interface ReserveFees {
-//   borrowFeeWad: BN;
-//   flashLoanFeeWad: BN;
-//   hostFeePercentage: BN;
-// }
-
-// interface ReserveCollateral {
-//   reserveTokenMint: PublicKey;
-//   mintTotalSupply: BN;
-//   supplyPubkey: PublicKey;
-// }
-
-// interface ReserveLiquidity {
-//   mintPubkey: PublicKey;
-//   mintDecimals: BN;
-//   supplyPubkey: PublicKey;
-//   feeReceiver: PublicKey;
-//   oraclePubkey: PublicKey;
-//   availableAmount: BN;
-//   borrowedAmount: BN;
-//   cumulativeBorrowRate: BN;
-//   marketPrice: BN;
-//   platformAmountWads: BN;
-//   platformFee: BN;
-// }
-
-// interface LastUpdate {
-//   lastUpdatedSlot: BN;
-//   stale: boolean;
-// }
-
-// export interface ReserveInfo extends IReserveInfo {
-//   version: BN;
-//   lastUpdate: LastUpdate;
-//   lendingMarket: PublicKey;
-//   borrowAuthorizer: PublicKey;
-//   liquidity: ReserveLiquidity;
-//   collateral: ReserveCollateral;
-//   config: ReserveConfig;
-// }
-
-// export interface VaultInfo extends IVaultInfo {
-//   base: Base;
-// }
-
-export async function getAllDepositors(connection: Connection, userKey?: PublicKey): Promise<types.DepositorInfo[]> {
-  const dataSizeFilters: DataSizeFilter = {
-    dataSize: DEPOSITOR_LAYOUT_SPAN,
-  };
-  let filters: (MemcmpFilter | DataSizeFilter)[] = [dataSizeFilters];
-  if (userKey) {
-    const programIdMemcmp: MemcmpFilter = {
-      memcmp: {
-        offset: 8,
-        bytes: userKey.toString(),
-      },
-    };
-    filters = [programIdMemcmp, dataSizeFilters];
-  }
-  const config: GetProgramAccountsConfig = { filters: filters };
-  const allDepositorAccounts = await connection.getProgramAccounts(TULIP_VAULT_V2_PROGRAM_ID, config);
-  let allDepositors: types.DepositorInfo[] = [];
-
-  for (let depositorAccountInfo of allDepositorAccounts) {
-    const vault = parseDepositor(depositorAccountInfo.account.data, depositorAccountInfo.pubkey);
-    allDepositors.push(vault);
-  }
-
-  return allDepositors;
-}
-
-export async function getDepositor(connection: Connection, depositorId: PublicKey): Promise<types.DepositorInfo> {
-  const depositorAccountInfo = await connection.getAccountInfo(depositorId);
-  const depositor = parseDepositor(depositorAccountInfo?.data, depositorId);
-
-  return depositor;
-}
-
-export function parseDepositor(data: any, depositorId: PublicKey): types.DepositorInfo {
-  const decodeData = DEPOSITOR_LAYOUT.decode(data);
-  const {
-    owner,
-    vault,
-    pdaNonce,
-    queueNonce,
-    shares,
-    depositedBalance,
-    lastDepositTime,
-    pendingWithdrawAmount,
-    totalDepositedUnderlying,
-    totalWithdrawnUnderlying,
-    lastPendingReward,
-    rewardPerSharePaid,
-    extra_data_account,
-  } = decodeData;
-
-  return {
-    depositorId,
-    userKey: owner,
-    vaultId: vault,
-    pdaNonce,
-    queueNonce,
-    shares,
-    depositedBalance,
-    lastDepositTime,
-    pendingWithdrawAmount,
-    totalDepositedUnderlying,
-    totalWithdrawnUnderlying,
-    lastPendingReward,
-    rewardPerSharePaid,
-    extra_data_account,
-  };
 }
--- test/tulip.ts
+++ test/tulip.ts
@@ -1,5 +1,6 @@
 import { PublicKey, Connection } from "@solana/web3.js";
 import * as tulip from "../src/tulip";
+import { getTokenList } from "../src/utils";
 
 describe("Tulip", () => {
   // const connection = new Connection("https://rpc-mainnet-fork.dappio.xyz", {
@@ -10,10 +11,6 @@ describe("Tulip", () => {
   //   commitment: "confirmed",
   //   confirmTransactionInitialTimeout: 180 * 1000,
   // });
-  const connection = new Connection("https://ssc-dao.genesysgo.net", {
-    commitment: "confirmed",
-    confirmTransactionInitialTimeout: 180 * 1000,
-  });
   // const connection = new Connection("https:////api.mainnet-beta.solana.com", {
   //   commitment: "confirmed",
   //   confirmTransactionInitialTimeout: 180 * 1000,
@@ -23,49 +20,68 @@ describe("Tulip", () => {
   //   confirmTransactionInitialTimeout: 180 * 1000,
   //   wsEndpoint: "wss://rpc-mainnet-fork.epochs.studio/ws",
   // });
+  const connection = new Connection("https://ssc-dao.genesysgo.net", {
+    commitment: "confirmed",
+    confirmTransactionInitialTimeout: 180 * 1000,
+  });
 
   const solReserveId = new PublicKey("8PbodeaosQP19SjYFx855UMqWxH2HynZLdBXmsrbac36");
+  const userKey = new PublicKey("G9on1ddvCc8xqfk2zMceky2GeSfVfhU8JqGHxNEWB5u4");
 
-  // it(" Can get all reserves", async () => {
-  //   const reserves = (await tulip.infos.getAllReserves(connection)) as tulip.ReserveInfo[];
-  //   console.log(`Fetched ${reserves.length} reserves`);
-  //   // console.log(`${reserves.map((r)=>token)}`);
-  // });
-  // it(" Can get all reserveWrappers", async () => {
-  //   const wrappers = (await tulip.infos.getAllReserveWrappers(connection)) as tulip.ReserveInfoWrapper[];
-  //   // const miningAPYs = await Promise.all(wrappers.map(async (w) => await w.miningApy(connection)));
-  //   // const supplyAPYs = await Promise.all(wrappers.map(async (w) => await w.supplyApy()));
-  //   console.log(`Fetched ${wrappers.length} reserveInfoWrapper`);
-  //   // console.log(`Pool with miningAPY count: ${miningAPYs.filter((a) => a).length}`);
-  //   // console.log(`Pool with supplyAPY count: ${supplyAPYs.filter((a) => a).length} `);
-  //   // console.log(
-  //   //   `reserves with partner rewards: ${wrappers.filter((w) => w.reserveInfo.partnerRewardData.length > 0).length}`
-  //   // );
-  // });
-  // it(" Can get reserve", async () => {
-  //   const reserve = (await tulip.infos.getReserve(connection, solReserveId)) as tulip.ReserveInfo;
-  //   console.log(
-  //     `Fetched reserve: ${JSON.stringify({
-  //       id: reserve.reserveId,
-  //       mint: reserve.liquidity.mintPubkey.toBase58(),
-  //     })}`
-  //   );
-  // });
+  it(" Can get all reserves", async () => {
+    const reserves = (await tulip.infos.getAllReserves(connection)) as tulip.ReserveInfo[];
+    console.log(`Fetched ${reserves.length} reserves`);
+  });
+  it(" Can get all reserveWrappers", async () => {
+    const tokenList = await getTokenList();
+    const wrappers = (await tulip.infos.getAllReserveWrappers(connection)) as tulip.ReserveInfoWrapper[];
+    const mints = await Promise.all(wrappers.map(async (w) => await w.reserveInfo.liquidity.mintPubkey.toBase58()));
+    console.log(`Fetched ${wrappers.length} reserveInfoWrapper`);
+    console.log(
+      `All pools' symbol:\n ${mints.map((m) => tokenList.find((t) => t.mint === m)?.symbol).filter((s) => s)}`
+    );
+  });
+  it(" Can get reserve", async () => {
+    const reserve = (await tulip.infos.getReserve(connection, solReserveId)) as tulip.ReserveInfo;
+    console.log(
+      `Fetched reserve: ${JSON.stringify({
+        id: reserve.reserveId,
+        mint: reserve.liquidity.mintPubkey.toBase58(),
+      })}`
+    );
+  });
   it(" Can get all vaults", async () => {
     const vaults = (await tulip.infos.getAllVaults(connection)) as tulip.VaultInfo[];
     console.log(`Fetched ${vaults.length} vaults`);
-    // vaults.forEach((v, i) => {
-    //   console.log(`\n* Vault#${i + 1}`);
-    //   console.log(`** VaultId: ${v.vaultId}`);
-    // });
+    vaults.forEach((v, i) => {
+      console.log(`\n* Vault#${i + 1}`);
+      console.log(`** VaultId: ${v.vaultId}`);
+    });
   });
   it(" Can get vault", async () => {
     const vault = (await tulip.infos.getVault!(
       connection,
       new PublicKey("4gKvh8AmET6U84KXAJdnq1eU53mtniPXGyfDTW2R3rQN")
-      // new PublicKey("BMBaSwtdHxeXaCW68oMGDr26kLoK2bPP8aSMrNgkm62U")
     )) as tulip.VaultInfo;
     console.log(`- Vault shareMint: ${vault.shareMint.toBase58()}`);
     console.log(`- Vault base PDA: ${vault.base.pda.toBase58()}`);
+  });
+  it(" Can get all depositors", async () => {
+    const depositors = (await tulip.infos.getAllDepositors(connection, userKey)) as tulip.DepositorInfo[];
+    console.log(`Fetched ${depositors.length} depositors`);
+    depositors.forEach((d, i) => {
+      console.log(`\n* Depositor#${i + 1}`);
+      console.log(`** DepositedId: ${d.vaultId}`);
+      console.log(`** Deposited Balance: ${d.depositedBalance.toNumber()}`);
+      console.log(`** Corresponding vaultId: ${d.vaultId.toBase58()}`);
+    });
+  });
+  it(" Can get depositor", async () => {
+    const depositor = (await tulip.infos.getDepositor(
+      connection,
+      new PublicKey("4gKvh8AmET6U84KXAJdnq1eU53mtniPXGyfDTW2R3rQN")
+    )) as tulip.DepositorInfo;
+    console.log(`- Deposited Balance: ${depositor.depositedBalance.toNumber()}`);
+    console.log(`- Corresponding vault: ${depositor.vaultId.toBase58()}`);
   });
 });
