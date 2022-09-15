697a079100e58f2ba31105bed2234830c5ad66a0
Wei <wei@dappio.xyz>
2022-09-16 06:56:06 +0800

    add friktion account info interfaces

---
+++ src/friktion/index.ts
@@ -0,0 +1,97 @@
+export * from "./ids";
+export * from "./infos";
+export * from "./layouts";
+import { PublicKey } from "@solana/web3.js";
+import { IDepositorInfo, IVaultInfo } from "../types";
+import BN from "bn.js";
+export interface VaultInfo extends IVaultInfo {
+  // vaultId
+  admin: PublicKey;
+  seed: PublicKey;
+  transferWindow: BN;
+  startTransferTime: BN;
+  endTransferTime: BN;
+  initialized: boolean;
+  currOptionWasSettled: boolean;
+  mustSwapPremiumToUnderlying: boolean;
+  nextOptionWasSet: boolean;
+  firstEverOptionWasSet: boolean;
+  instantTransfersEnabled: boolean;
+  prepareIsFinished: boolean;
+  enterIsFinished: boolean;
+  roundHasStarted: boolean;
+  roundNumber: BN;
+  totalUnderlyingPreEnter: BN;
+  totalUnderlyingPostSettle: BN;
+  totalVoltTokensPostSettle: BN;
+  vaultAuthority: PublicKey;
+  depositPool: PublicKey;
+  premiumPool: PublicKey;
+  optionPool: PublicKey;
+  writerTokenPool: PublicKey;
+  vaultMint: PublicKey; // shareMint
+  underlyingAssetMint: PublicKey;
+  quoteAssetMint: PublicKey;
+  optionMint: PublicKey;
+  writerTokenMint: PublicKey;
+  optionMarket: PublicKey;
+  vaultType: BN;
+  underlyingAmountPerContract: BN;
+  quoteAmountPerContract: BN;
+  expirationUnixTimestamp: BN;
+  expirationInterval: BN;
+  upperBoundOtmStrikeFactor: BN;
+  haveTakenWithdrawalFees: boolean;
+  serumSpotMarket: PublicKey;
+  openOrdersBump: number;
+  openOrdersInitBump: number;
+  ulOpenOrdersBump: number;
+  ulOpenOrders: PublicKey;
+  ulOpenOrdersInitialized: boolean;
+  bumpAuthority: number;
+  serumOrderSizeOptions: BN;
+  individualCapacity: BN;
+  serumOrderType: BN;
+  serumLimit: number;
+  serumSelfTradeBehavior: number;
+  serumClientOrderId: BN;
+  whitelistTokenMint: PublicKey;
+  permissionedMarketPremiumMint: PublicKey;
+  permissionedMarketPremiumPool: PublicKey;
+  capacity: BN;
+  roundInfos: RoundInfo[];
+}
+
+export interface DepositorInfo extends IDepositorInfo {
+  // depositorId
+  // userKey
+  type: DepositorType;
+  initialized: boolean;
+  roundNumber: BN;
+  amount: BN;
+}
+
+export interface ExtraVaultInfo {
+  vaultId: PublicKey;
+  isWhitelisted: boolean;
+  whitelist: PublicKey;
+  isForDao: boolean;
+  daoProgramId: PublicKey;
+  depositMint: PublicKey;
+  targetLeverage: BN;
+  targetLeverageLenience: BN;
+}
+
+export enum DepositorType {
+  PendingDeposit,
+  PendingWithdrawal,
+}
+export interface RoundInfo {
+  roundId: PublicKey;
+  roundNumber: BN;
+  underlyingFromPendingDeposits: BN;
+  voltTokensFromPendingWithdrawals: BN;
+  underlyingPreEnter: BN;
+  underlyingPostSettle: BN;
+  premiumFarmed: BN;
+}
