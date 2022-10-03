a458dffd870960dab0c37f3ed62fb56368f999e7
Alvin Philips <alvinphilips257@gmail.com>
2022-10-04 05:08:27 +0530

    feat: Set up Lido Finance SDK, with basic data

--- src/lido/index.ts
+++ src/lido/index.ts
@@ -1,1 +1,13 @@
+export * from "./ids";
 export * from "./infos";
+
+import BN from "bn.js";
+import { IDepositorInfo, IVaultInfo } from "../types";
+
+// TODO: Add relevant fields
+export interface VaultInfo extends IVaultInfo {
+  amount: BN;
+}
+
+// TODO: Add relavant fields
+export interface DepositorInfo extends IDepositorInfo {}
