89926f08001f2b2ded098d9a7bc1d7514b4ed12c
ayushpaharia <ayush.paharia.18@gmail.com>
2022-10-09 17:27:06 +0530

    feat: added infos for marinade vault

--- src/marinade/ids.ts
+++ src/marinade/ids.ts
@@ -4,13 +4,7 @@ import { PublicKey } from "@solana/web3.js";
 
 export const MARINADE_PROGRAM_ID = new PublicKey("MarBmsSgKXdrN1egZf5sqe1TMai9K1rChYNDJgjq7aD");
 
-// make layouts out of ids
+export const MSOL_MINT_ADDRESS = new PublicKey("mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So");
 
-// make types/interfaces inside index.ts ( Marinade ) from layouts.ts ( Marinade )
-
-// make parseVault in infos.ts
-// make getVault
-
-// fetch Data from accountId and put data through parsing fxn
 // accountId
 // 8szGkuLTAux9XMgZ2vtY39jVSowEcpBfFfD8hXSEqdGC
--- src/marinade/infos.ts
+++ src/marinade/infos.ts
@@ -1,28 +1,149 @@
-import {
-  Connection,
-  MemcmpFilter,
-  GetProgramAccountsConfig,
-  DataSizeFilter,
-  PublicKey,
-  AccountInfo,
-} from "@solana/web3.js";
+import { Connection, PublicKey } from "@solana/web3.js";
 
-import { TOKEN_PROGRAM_ID, AccountLayout } from "@solana/spl-token-v2";
+import { MARINADE_PROGRAM_ID, MSOL_MINT_ADDRESS } from "./ids";
 
-import { IInstanceVault } from "../types";
+import { IInstanceVault, IVaultInfoWrapper } from "../types";
+import { MARINADE_FINANCE_ACCOUNT_STATE } from "./layouts";
+
+import * as types from ".";
+import { ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID } from "@solana/spl-token-v2";
 
 let infos: IInstanceVault;
 
 infos = class InstanceMarinade {
-  static async getVaultInfo() {}
-  static async getAllVaults(): Promise<any> {}
-  static async getAllVaultWrappers(): Promise<any> {}
-  static async getVault(): Promise<any> {}
-  static parseVault(): any {}
-  static async getAllDepositors(): Promise<any> {}
-  static async getDepositor(): Promise<any> {}
-  static getDepositorId(): any {}
-  static parseDepositor(): any {}
+  static async getAllVaults(connection: Connection): Promise<types.VaultInfo[]> {
+    const accountInfos = await connection.getAccountInfo(MARINADE_PROGRAM_ID);
+
+    if (!accountInfos) throw Error("Error: Could not get solido address");
+    const vault = this.parseVault(accountInfos.data, MARINADE_PROGRAM_ID);
+
+    return [vault];
+  }
+
+  static async getAllVaultWrappers(connection: Connection): Promise<VaultInfoWrapper[]> {
+    return (await this.getAllVaults(connection)).map((vault) => new VaultInfoWrapper(vault));
+  }
+
+  static async getVault(connection: Connection, vaultId: PublicKey): Promise<types.VaultInfo> {
+    if (!vaultId.equals(MARINADE_PROGRAM_ID))
+      throw Error(`Error: Lido vaultId must match ${MARINADE_PROGRAM_ID.toBase58()}`);
+
+    const vaultAccountInfo = await connection.getAccountInfo(vaultId);
+    if (!vaultAccountInfo) throw Error("Error: Cannot get marinade token account");
+
+    const vault: types.VaultInfo = this.parseVault(vaultAccountInfo.data, vaultId);
+
+    return vault;
+  }
+
+  static parseVault(data: Buffer, vaultId: PublicKey): types.VaultInfo {
+    // Decode the Token data using AccountLayout
+    const decodeData = MARINADE_FINANCE_ACCOUNT_STATE.decode(data);
+    const {
+      msolMint,
+      adminAuthority,
+      operationalSolAccount,
+      treasuryMsolAccount,
+      reserveBumpSeed,
+      msolMintAuthorityBumpSeed,
+      rentExemptForTokenAcc,
+      rewardFee,
+      stakeSystem,
+      validatorSystem,
+      liqPool,
+      availableReserveBalance,
+      msolSupply,
+      msolPrice,
+      circulatingTicketCount,
+      circulatingTicketBalance,
+      lentFromReserve,
+      minDeposit,
+      minWithdraw,
+      stakingSolCap,
+      emergencyCoolingDown,
+    } = decodeData;
+
+    // Ensure the mint matches
+    if (!this._isAllowed(msolMint)) {
+      throw Error("Error: Not a mSOL token account");
+    }
+
+    return {
+      vaultId,
+      msolMint,
+      adminAuthority,
+      operationalSolAccount,
+      treasuryMsolAccount,
+      reserveBumpSeed,
+      msolMintAuthorityBumpSeed,
+      rentExemptForTokenAcc,
+      rewardFee,
+      stakeSystem,
+      validatorSystem,
+      liqPool,
+      availableReserveBalance,
+      msolSupply,
+      msolPrice,
+      circulatingTicketCount,
+      circulatingTicketBalance,
+      lentFromReserve,
+      minDeposit,
+      minWithdraw,
+      stakingSolCap,
+      emergencyCoolingDown,
+    } as types.VaultInfo;
+  }
+
+  static async getAllDepositors(connection: Connection, userKey: PublicKey): Promise<types.DepositorInfo[]> {
+    const { value: accountInfos } = await connection.getTokenAccountsByOwner(userKey, {
+      mint: MSOL_MINT_ADDRESS,
+    });
+    const depositors = accountInfos.map((depositor) => this.parseDepositor(depositor.account.data, depositor.pubkey));
+    return depositors;
+  }
+
+  static async getDepositor(connection: Connection, depositorId: PublicKey): Promise<types.DepositorInfo> {
+    const depositorAccountInfo = await connection.getAccountInfo(depositorId);
+    if (!depositorAccountInfo) throw Error("Error: Cannot get depositor account");
+
+    return this.parseDepositor(depositorAccountInfo.data, depositorId);
+  }
+
+  static getDepositorId(
+    _vaultId: PublicKey,
+    userKey: PublicKey,
+    _programId: PublicKey = MARINADE_PROGRAM_ID
+  ): PublicKey {
+    return PublicKey.findProgramAddressSync(
+      [userKey.toBuffer(), TOKEN_PROGRAM_ID.toBuffer(), MSOL_MINT_ADDRESS.toBuffer()],
+      ASSOCIATED_TOKEN_PROGRAM_ID
+    )[0];
+  }
+
+  static parseDepositor(data: Buffer, depositorId: PublicKey): types.DepositorInfo {
+    const decodeData = MARINADE_FINANCE_ACCOUNT_STATE.decode(data);
+    const { mint, amount, owner } = decodeData;
+
+    // Ensure the mint matches
+    if (!this._isAllowed(mint)) {
+      throw Error(`Error: Not a stSOL token account`);
+    }
+
+    return {
+      depositorId,
+      userKey: owner,
+      amount,
+      mint,
+    };
+  }
+
+  private static _isAllowed(mint: PublicKey): boolean {
+    return mint.equals(MSOL_MINT_ADDRESS);
+  }
 };
 
 export { infos };
+
+export class VaultInfoWrapper implements IVaultInfoWrapper {
+  constructor(public vaultInfo: types.VaultInfo) {}
+}
