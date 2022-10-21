14838304176ba11f0bc1a107fa88eef0c7e74b46
Alvin Philips <alvinphilips257@gmail.com>
2022-10-21 14:50:23 +0530

    chore: deleted unused comments

--- src/lido/ids.ts
+++ src/lido/ids.ts
@@ -1,12 +1,5 @@
 import { PublicKey } from "@solana/web3.js";
 
-// Mainnet addresses
 export const LIDO_PROGRAM_ID = new PublicKey("CrX7kMhLC3cSsXJdT7JDgqrRVWGnUpX3gfEfxxU2NVLi");
 export const LIDO_ADDRESS = new PublicKey("49Yi1TKkNyYjPAFdR9LBvoHcUjuPX4Df5T5yv39w2XTn");
 export const ST_SOL_MINT_ADDRESS = new PublicKey("7dHbWXmci3dT8UFYWYZweBLXgycu7Y3iL6trKn1Y7ARj");
-
-// Testnet addresses
-
-// export const LIDO_PROGRAM_ID = new PublicKey("6RRggYnFe2EcD543QXrE3Wxp1Kgcq8qctwRrNnvjoYsL");
-// export const LIDO_ADDRESS = new PublicKey("Hcqw2G2FkBhBiDZz3PXFBUKuMQiKMJmXvcfqRaXkyNXF");
-// export const ST_SOL_MINT_ADDRESS = new PublicKey("Bz2UPiXJmCQYT8XQHaPPXG3fywNp2TENav5dcJdX9q21");
