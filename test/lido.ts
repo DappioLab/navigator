ddfbea5506320c4c788ae0cceea53371a936d82e
Alvin Philips <alvinphilips257@gmail.com>
2022-10-21 14:50:23 +0530

    chore: deleted unused comments

--- test/lido.ts
+++ test/lido.ts
@@ -8,12 +8,6 @@ describe("Lido", () => {
     confirmTransactionInitialTimeout: 180 * 1000,
   });
 
-  // Use Testnet
-  // const connection = new Connection("https://api.testnet.solana.com", {
-  //   commitment: "confirmed",
-  //   confirmTransactionInitialTimeout: 180 * 1000,
-  // });
-
   //TODO: Modify these addresses
   const userKey = new PublicKey("Dkx85wVaUaDy9i9XWFdZvYrw5h2WMP1N8TqXPBdXY5Wh");
   const tokenKey = new PublicKey("F9kWLTs28mWKmmvKDhAvtHuVwYKx6L4yG7C3n4WyLVh6");
