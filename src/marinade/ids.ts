b280adbda72d7578bfe0a6f33d4fb62811b50c74
ayushpaharia <ayush.paharia.18@gmail.com>
2022-10-25 18:29:33 +0530

    feat: add marinade ids

---
+++ src/marinade/ids.ts
@@ -0,0 +1,7 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const MARINADE_PROGRAM_ID = new PublicKey("MarBmsSgKXdrN1egZf5sqe1TMai9K1rChYNDJgjq7aD");
+
+export const MSOL_MINT_ADDRESS = new PublicKey("mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So");
+
+export const MSOL_VAULT_ADDRESS = new PublicKey("8szGkuLTAux9XMgZ2vtY39jVSowEcpBfFfD8hXSEqdGC");
