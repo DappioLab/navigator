4acc27aa9838b5bd8aac06e4920ba491c3241153
emerson <emerson@dappio.xyz>
2022-12-11 16:08:44 +0800

    Add patch for Raydium vault

---
+++ src/tulip/ids/patch.ts
@@ -0,0 +1,16 @@
+export const patch = {
+  // RAY-SOL
+  HUDr9BDaAGqi37xbQHzxCyXvfMCKPTPNF8g9c9bPu1Fu: "AVs9TA4nWDzfPJE9gGVNJMVhcQy3V9PGazuz33BfG2RA",
+  // RAY-USDC (OpenBook)
+  CHYrUBX2RKX8iBg7gYTkccoGNBzP44LdaazMHCLcdEgS: "6UmmUiYoBjSrhakAobJw8BvkmJtDVxaeBtbt7rxWo1mg",
+  // RAY-SRM
+  "5DFbcYNLLy5SJiBpCCDzNSs7cWCsUbYnCkLXzcPQiKnR": "GaqgfieVmnmY4ZsZHHA6L5RSVzCGL3sKx4UgHBaYNy8m",
+  // RAY-USDT (OpenBook)
+  AvbVWpBi2e4C9HPmZgShGdPoNydG4Yw8GJvG9HUcLgce: "DVa7Qmb5ct9RCpaU7UTpSaf3GVMYz17vNVU67XpdCRut",
+  // ATLAS-RAY
+  BHHhNLdJn69K1XPJcpcw4MBY3TPetpLxhj8s4K4ydsDV: "F73euqPynBwrgcZn3fNSEneSnYasDQohPM5aZazW9hp2",
+  // RAY-ETH
+  B6fbnZZ7sbKHR18ffEDD5Nncgp54iKN1GbCgjTRdqhS1: "8iQFhWyceGREsWnLM8NkG9GC8DvZunGZyMzuyUScgkMK",
+  // RAY-whETH
+  CV2WzYEphJnNFfLLRinDo9BqfpT3rRhxEwbaw1RiKnyQ: "E9Bg7Fg5TVSXge9ADN7fDjsR7MXRKsgq3STJ9aCdZoTZ",
+};
