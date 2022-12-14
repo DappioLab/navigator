ce18322a86189b4776472eb2aa32599ac763a786
Wei <wei@dappio.xyz>
2022-12-14 17:07:21 +0800

    add option program id

--- src/friktion/ids.ts
+++ src/friktion/ids.ts
@@ -1,12 +1,8 @@
 import { PublicKey } from "@solana/web3.js";
 
-export const VOLT_PROGRAM_ID = new PublicKey(
-  "VoLT1mJz1sbnxwq5Fv2SXjdVDgPXrb9tJyC8WpMDkSp"
-);
+export const VOLT_PROGRAM_ID = new PublicKey("VoLT1mJz1sbnxwq5Fv2SXjdVDgPXrb9tJyC8WpMDkSp");
 
-export const VOLT_FEE_OWNER = new PublicKey(
-  "3KjJiWBfaw96qGhysq6Fc9FTxdPgPTNY6shM7Bwfp8EJ"
-);
+export const VOLT_FEE_OWNER = new PublicKey("3KjJiWBfaw96qGhysq6Fc9FTxdPgPTNY6shM7Bwfp8EJ");
 
 export const STABLE_VAULT_IDS = [
   "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
@@ -14,3 +10,5 @@ export const STABLE_VAULT_IDS = [
   "7kbnvuGBxxj8AG9qp8Scn56muWGaRaFqxg1FsRp3PaFT",
   "Cvvh8nsKZet59nsDDo3orMa3rZnPWQhpgrMCVcRDRgip",
 ];
+
+export const INERTIA_PROGRAM_ID = new PublicKey("iNeq88RkcvbwfnWAsqw5rQvJTbuKEA15PaMe7YAcTx3");
